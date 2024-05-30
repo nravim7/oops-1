1bed4494eac8da3866e8efdc86d3c96375e12706
Ravi N <your.email@example.com>
2024-05-30 08:00:50 +0530

    Car Class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
 
+        Car car = new Car();
+
+        car.setMake("Porsche");
+        car.setModel("Carrera");
+        car.setDoors(2);
+        car.setConvertible(true);
+        car.setColor("Black");
+
+        System.out.println("make = " + car.getMake());
+        System.out.println("model = " + car.getModel());
+        System.out.println("doors = " + car.getDoors());
+        System.out.println("Convertible = " + car.isConvertible());
+        car.describeCar();
+
     }
 }
